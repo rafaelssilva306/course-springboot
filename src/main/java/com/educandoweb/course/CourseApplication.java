9fc55523014d93ba8b46c88741fb239af4ad8bb6
Rafael Silva <rafa.306@hotmail.com>
2021-09-06 19:27:53 -0300

    Product

--- src/main/java/com/educandoweb/course/CourseApplication.java
+++ src/main/java/com/educandoweb/course/CourseApplication.java
@@ -1,3 +1,4 @@
+
 package com.educandoweb.course;
 
 import org.springframework.boot.SpringApplication;
--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -1,5 +1,5 @@
 package com.educandoweb.course.entities;
-
+ 
 import java.io.Serializable;
 import java.time.Instant;
 
@@ -11,58 +11,74 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-
+import com.educandoweb.course.entities.enums.OrderStatus;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+ 
 @Entity
 @Table(name = "tb_order")
+@JsonPropertyOrder({"id", "orderStatus","moment" })
 public class Order implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+ 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd't'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
+	@JsonIgnore
+	private Integer orderStatus;
 	
+    @JsonIgnore
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
 	
-	public Order() {	
+	public Order() {
+ 
 	}
-
-	public Order(Long id, Instant moment, User client) {
+ 
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
-
+ 
 	public Long getId() {
 		return id;
 	}
-
+ 
 	public void setId(Long id) {
 		this.id = id;
 	}
-
+ 
 	public Instant getMoment() {
 		return moment;
 	}
-
+ 
 	public void setMoment(Instant moment) {
 		this.moment = moment;
 	}
-
+ 
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+ 
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if(orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
+	}
+ 
 	public User getClient() {
 		return client;
 	}
-
+ 
 	public void setClient(User client) {
 		this.client = client;
 	}
-
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -70,7 +86,7 @@ public class Order implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
-
+ 
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -87,6 +103,7 @@ public class Order implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
+ 
+ 
+ 
 }
